822f33838ff5b7bc4ae28d40583c08a2afa43d15
BrRoman <editeur@traditions-monastiques.com>
2021-05-24 21:02:24 +0200

    OK.

--- missa/apps/main/forms.py
+++ missa/apps/main/forms.py
@@ -1,19 +1,29 @@
 """ apps/main/forms.py """
 
+from datetime import date
+
 from django import forms
+from django.forms.fields import CharField, DateField
+from django.forms.widgets import HiddenInput
 
 from .models import Day
 
 
+class DateChoiceField(forms.ModelChoiceField):
+    def label_from_instance(self, obj):
+        return '{}'.format(obj.date_french())
+
+
 class DayForm(forms.ModelForm):
     """ Form for Day. """
-    destination = forms.CharField(
+    day = DateField(
+        widget=HiddenInput(),
+    )
+    father = CharField(
         required=False,
-        widget=forms.TextInput(
-            attrs={
-                'class': 'w-100',
-            },
-        ),
+    )
+    brother = CharField(
+        required=False,
     )
 
     class Meta:
